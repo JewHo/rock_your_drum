ce0adc2428aa52d3156e10c5f894f0046a2ffb81
Juho <juho.a.rinne@gmail.com>
2022-03-02 21:33:29 +0200

    jannedrum

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>ROCK YOUR DRUM!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+       <img src="/jannedrum.png" />
       </main>
     </div>
   )
